4f7f5700231f6d314dc7ee64e448ff84757a85e7
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-08 22:50:04 +0600

    service-detail-with-dynamic-id-updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 //middleware
@@ -21,13 +21,30 @@ async function run() {
   const photographyCollection = client
     .db("photography")
     .collection("photography-reviews");
-
-  app.get("/photographs", async (req, res) => {
-    const query = {};
-    const cursor = photographyCollection.find(query);
-    const result = await cursor.toArray();
-    res.send(result);
-  });
+  try {
+    //get all data
+    app.get("/photographs", async (req, res) => {
+      const query = {};
+      const cursor = photographyCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //get limited data
+    app.get("/photoCount", async (req, res) => {
+      const query = {};
+      const cursor = photographyCollection.find(query).limit(3);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //get data by id
+    app.get("/photographs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await photographyCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
 }
 
 run().catch((err) => {
